18eac9216df8e7446a6b18f2ae5f8c7c8032c312
c0d3m0nk3y <rickyoswald@gmail.com>
2016-01-22 02:15:24 +0000

    Added option to print all terms or just target term

--- printterms.py
+++ printterms.py
@@ -5,19 +5,25 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("targetTerm", help="The number of terms to print", type=int)
+parser.add_argument("-a", "--printAll", help="Print all of the terms", type=bool)
 args = parser.parse_args()
 
 currentTerm = 0
 a = 0
 b = 1
 
+strings = []
 start = time.time()
 while(currentTerm < args.targetTerm):
- print("Term ", currentTerm + 1, " = ", a)
+ strings.append("Term %s = %s" % (currentTerm + 1, a))
  new = a + b
  a = b
  b = new
  
  currentTerm = currentTerm + 1
  
-print("\nTime : ", time.time() - start)
+if(args.printAll):
+ print("\n".join(strings), "\nCalculated in : ", time.time() - start)
+ print("Printed in : ", time.time() - start)
+else:
+ print("Term ", args.targetTerm, " = ", b - a, "\nCalculated in : ", time.time() - start)
