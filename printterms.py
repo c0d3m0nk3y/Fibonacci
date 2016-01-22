8401ebce4f626a06c1dd84de076bea06aa0e7d23
c0d3m0nk3y <rickyoswald@gmail.com>
2016-01-22 01:21:46 +0000

    User can specify target term with args

--- printterms.py
+++ printterms.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--targetTerm", help="The number of terms to print", type=int)
+args = parser.parse_args()
+
 currentTerm = 0
 a = 0
 b = 1
-while(currentTerm < 10):
+
+while(currentTerm < args.targetTerm):
  print("Term ", currentTerm + 1, " = ", a)
  new = a + b
  a = b
